f0c16861f4b23c4509c41e1f8ca251e190131bfd
Targol Hosseini <targol.hosseini@ferocia.com.au>
2021-11-23 10:13:40 +1100

    Initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  get  'static_pages/home'
-  get  'static_pages/help'
   get  'static_pages/about'
+  get  'static_pages/contact'
+  get  'static_pages/help'
+  get  'static_pages/home'
+  # get  'static_pages_contact_url/root_url'
   # root 'application#hello'
+  # root 'static_pages#home'
 end
